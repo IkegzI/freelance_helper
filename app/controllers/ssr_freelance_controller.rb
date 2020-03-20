1b2dc0411903678aa2a34ead9f684861627da8b4
IkegzI <kegz@mail.ru>
2020-03-20 18:56:33 +0800

    200320/3

--- app/controllers/ssr_freelance_controller.rb
+++ app/controllers/ssr_freelance_controller.rb
@@ -70,6 +70,8 @@ class SsrFreelanceController < ApplicationController
       end
       user_pay_wallet = user.custom_values.find_by(custom_field_id: custom_field_wallet.id) || ''
       user_pay_type = user.custom_values.find_by(custom_field_id: custom_field_type.id) || ''
+      binding.pry
+
       if params[:issue_id] != '' and change_assigned_user
         if user_pay_wallet.value == ''
           user_pay_wallet = Issue.find(params[:issue_id]).custom_values.find_by(custom_field_id: custom_field_wallet_issue.id)
@@ -82,11 +84,14 @@ class SsrFreelanceController < ApplicationController
     if project
       role_ids_custom = SsrFreelanceSetting.all.map { |item| item.role_id }.compact
       check = (Member.where(user_id: user.id).find_by(project_id: project.id).role_ids.map { |item| true if role_ids_custom.include?(item) }).compact.pop if Member.where(user_id: user.id).find_by(project_id: project.id)
+      binding.pry
+
     end
     # if check != []
       a << {number: custom_field_wallet_issue.id, value: user_pay_wallet == '' ? '' : user_pay_wallet.value}
       a << {number: custom_field_type_issue.id, value: user_pay_type == '' ? '' : user_pay_type.value}
       a << {number: 666, value: check}
+    binding.pry
     # else
     #   a << {number: custom_field_wallet_issue.id, value: ''}
     #   a << {number: custom_field_type_issue.id, value: ''}
