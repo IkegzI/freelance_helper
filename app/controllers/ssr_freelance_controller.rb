15c737c7d62bdc432361d4111a01c32871fc0b12
IkegzI <kegz@mail.ru>
2020-03-20 21:18:33 +0800

    200320/7

--- app/controllers/ssr_freelance_controller.rb
+++ app/controllers/ssr_freelance_controller.rb
@@ -61,6 +61,7 @@ class SsrFreelanceController < ApplicationController
     custom_field_wallet_issue = CustomField.where(type: 'IssueCustomField').find(Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_issue_field_id'].to_i)
     custom_field_type_issue = CustomField.where(type: 'IssueCustomField').find(Setting.plugin_freelance_helper['sunstrike_freelance_pay_issue_field_id'].to_i)
     a = []
+    binding.pry
     if params['check_user_id']
       user = User.find(params['check_user_id'].to_i)
       if params['project_select'] == ''
@@ -70,11 +71,11 @@ class SsrFreelanceController < ApplicationController
       end
       user_pay_wallet = user.custom_values.find_by(custom_field_id: custom_field_wallet.id) || ''
       user_pay_type = user.custom_values.find_by(custom_field_id: custom_field_type.id) || ''
-      if params[:issue_id] != '' and !change_assigned_user
+      binding.pry
+      if params[:issue_id] != '' and change_assigned_user
         if user_pay_wallet.value == ''
           user_pay_wallet = Issue.find(params[:issue_id]).custom_values.find_by(custom_field_id: custom_field_wallet_issue.id)
         end
-        binding.pry
         if user_pay_type.value == ''
           user_pay_type = Issue.find(params[:issue_id]).custom_values.find_by(custom_field_id: custom_field_type_issue.id)
         end
@@ -89,7 +90,6 @@ class SsrFreelanceController < ApplicationController
       a << {number: custom_field_wallet_issue.id, value: user_pay_wallet == '' ? '' : user_pay_wallet.value}
       a << {number: custom_field_type_issue.id, value: user_pay_type == '' ? '' : user_pay_type.value}
       a << {number: 666, value: check}
-    binding.pry
     # else
     #   a << {number: custom_field_wallet_issue.id, value: ''}
     #   a << {number: custom_field_type_issue.id, value: ''}
