bf95faf121e047a404f06da8d3297b10654b2ae1
IkegzI <kegz@mail.ru>
2020-03-21 18:47:32 +0800

    210320/2

--- lib/ssr_freelance/hook/src/tz1.rb
+++ lib/ssr_freelance/hook/src/tz1.rb
@@ -22,27 +22,29 @@ end
 
 
 def freelance_change_on(data)
-  check = false
+  check = true
+  field_id = Setting.plugin_freelance_helper['sunstrike_freelance_field_id']
+  binding.pry
   data[:issue].custom_field_values.each do |item|
-    if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_field_id'].to_i
-      check = true if item.value == '1' and item.value_was != '1'
+    if item.custom_field.id == field_id.to_i
+      check = false if data[:params][:issue][:custom_field_values][field_id] == '1'
     end
   end
+  check
 end
 
 
 def freelance_off(data)
-  if change_assigned(data)
+  if change_assigned(data) and payment_info_non_change(data)
     data[:issue].custom_field_values.each do |item|
       if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_field_id'].to_i
         unless field_check_complete(data)
-          if payment_data(data)
-            item.value = '0'
-          end
+          item.value = '0'
         end
       end
-      payment_info_destroy(item) if change_assigned(data) and payment_data(data)
+      # if change_assigned(data) and change_payment_data(data)
     end
+    data = payment_info_destroy(data)
   end
   data
 end
@@ -61,43 +63,62 @@ def payment_info_add(item, usr)
   item
 end
 
-def payment_info_destroy(item)
-  if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_pay_issue_field_id'].to_i
-    item.value = ''
-  end
-  if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_issue_field_id'].to_i
-    item.value = ''
-  end
-end
-
-def unchange_payment_info(data)
-  check = false
+def payment_info_destroy(data)
   data[:issue].custom_field_values.each do |item|
     if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_pay_issue_field_id'].to_i
-      if item.value == item.value_was or item.value == ''
-        check = true
-      end
+      item.value = ''
     end
     if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_issue_field_id'].to_i
-      if item.value == item.value_was or item.value == ''
-        check = true
+      item.value = ''
+    end
+  end
+  data
+end
+
+def payment_info_non_change(data)
+  check = []
+  check[0] = false
+  pay_issue_id = Setting.plugin_freelance_helper['sunstrike_freelance_pay_issue_field_id'].to_i
+  pay_wallet_id = Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_issue_field_id'].to_i
+  pay_issue_value = Issue.find(data[:issue].id).custom_field_values.map { |item| item.value if item.custom_field.id == pay_issue_id }.compact.pop
+  pay_wallet_value = Issue.find(data[:issue].id).custom_field_values.map { |item| item.value if item.custom_field.id == pay_wallet_id }.compact.pop
+  data[:issue].custom_field_values.each do |item|
+    if item.custom_field.id == pay_issue_id
+      binding.pry
+      if item.value == pay_issue_value
+        check[0] = true
+      end
+    end
+    if item.custom_field.id == pay_wallet_id
+      binding.pry
+      if item.value == pay_wallet_value
+        check[1] = true
       end
     end
   end
-  check
+  binding.pry
+  if check.uniq.size == 1 and check.first
+    return true
+  else
+    return false
+  end
 end
 
-def payment_data(data)
+def change_payment_data(data)
   check = true
-  data[:issue].custom_field_values.each do |item|
-    if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_pay_issue_field_id'].to_i
-      if item.value != ''
-        check = false
+  if data[:params][:issue][:custom_field_values]
+    pay_issue_id = Setting.plugin_freelance_helper['sunstrike_freelance_pay_issue_field_id']
+    pay_wallet_id = Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_issue_field_id']
+    data[:issue].custom_field_values.each do |item|
+      if item.custom_field.id == pay_issue_id.to_i
+        if item.value == data[:params][:issue][:custom_field_values][pay_issue_id]
+          check = false
+        end
       end
-    end
-    if item.custom_field.id == Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_issue_field_id'].to_i
-      if item.value != ''
-        check = false
+      if item.custom_field.id == pay_wallet_id.to_i
+        if item.value == data[:params][:issue][:custom_field_values][pay_wallet_id]
+          check = false
+        end
       end
     end
   end
