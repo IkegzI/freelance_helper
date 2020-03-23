10ea90cd817c0280d763b540db75e1097cbfadf1
IkegzI <kegz@mail.ru>
2020-03-23 21:35:27 +0800

    230320/5

--- lib/ssr_freelance/patches/src/check_payment_info.rb
+++ lib/ssr_freelance/patches/src/check_payment_info.rb
@@ -9,12 +9,42 @@ def deny_edit_payments_details
       payment: Setting.plugin_freelance_helper['sunstrike_freelance_pay_issue_field_id'].to_i,
       wallet: Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_issue_field_id'].to_i
   }
+  data_cf = {}
+  data_cf = payment_info_user_data
+  data_cf[:issue_payment] = ''
+  data_cf[:issue_wallet] = ''
   custom_field_values.each do |item|
-    if id_cf.values.include?(item.custom_field.id) and (item.value != item.value_was) and item.value_was != ''
-      check = true
+    if id_cf[:payment] == item.custom_field_id
+      data_cf[:issue_payment] = item.value
+    end
+    if id_cf[:wallet] == item.custom_field_id
+      data_cf[:issue_wallet] = item.value
     end
   end
+  binding.pry
+  if data_cf[:issue_payment] != data_cf[:user_payment]
+    check = true
+  end
+  if data_cf[:issue_wallet] != data_cf[:user_wallet]
+    check = true
+  end
+  # if (item.value != item.value_was) and item.value_was != ''
+  #   check = true
+  # end
   check
+end
+
+def payment_info_user_data
+  id_cf = {
+      payment: Setting.plugin_freelance_helper['sunstrike_freelance_pay_user_field_id'].to_i,
+      wallet: Setting.plugin_freelance_helper['sunstrike_freelance_pay_wallet_user_field_id'].to_i
+  }
+  user_payment = User.find(assigned_to_id).custom_values.find_by(custom_field_id: id_cf[:payment]).value
+  user_wallet = User.find(assigned_to_id).custom_values.find_by(custom_field_id: id_cf[:wallet]).value
+  {
+      user_payment: user_payment,
+      user_wallet: user_wallet
+  }
 end
 
 def payments_details_check_add
