235e288ee4be1a43e1abb1e1e11e21e8e1f8ddab
IkegzI <kegz11@mail.ru>
2020-04-04 18:54:02 +0800

    040420

--- lib/ssr_freelance/patches/src/check_payment_info.rb
+++ lib/ssr_freelance/patches/src/check_payment_info.rb
@@ -21,12 +21,18 @@ def deny_edit_payments_details
       data_cf[:issue_wallet] = item.value
     end
   end
+  binding.pry
+
   if data_cf[:issue_payment] != data_cf[:user_payment]
     check = true
   end
+  binding.pry
+
   if data_cf[:issue_wallet] != data_cf[:user_wallet]
     check = true
   end
+  binding.pry
+
   # if (item.value != item.value_was) and item.value_was != ''
   #   check = true
   # end
